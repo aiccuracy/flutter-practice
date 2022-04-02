de6b5c3471b6551c957acec7ca35ad335bf5c9ba
aiccuracy <soojin.emily@gmail.com>
2022-04-03 00:05:13 +0900

    feat: add contact list with stateful widget

--- contacts/lib/main.dart
+++ contacts/lib/main.dart
@@ -1,28 +1,59 @@
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = 'Contacts App';
+  var contactNames = ["안수진", "정한비", "류가형"];
+  var liked = [0, 0, 0];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-            child: Container(
-              height: 50.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Icon(Icons.phone), Icon(Icons.message), Icon(Icons.contact_page)],
-              ),
-            ),
+          floatingActionButton: FloatingActionButton(
+            child: Text(a.toString()),
+            onPressed: () {
+              setState(() {
+                a++;
+              });
+            },
+          ),
+          appBar: AppBar(
+            title: Text(name),
+          ),
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, i) {
+              return ListTile(
+                leading: Text(liked[i].toString()),
+                title: Text(contactNames[i]),
+                trailing: IconButton(
+                  icon: Icon(Icons.favorite_outline_rounded),
+                  onPressed: () {
+                    setState(() {
+                      liked[i]++;
+                    });
+                  },
+                ),
+              );
+            },
+          )
         ),
-      ),
     );
   }
 }
+
